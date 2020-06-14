9dc29c5dc2e31b303d2bdc56db3fc8d2680a64c5
Tamal Saha <tamal@appscode.com>
2020-06-14 00:49:39 -0700

    Don't update repo index as it should be already up-to-date
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- hack/publish.sh
+++ hack/publish.sh
@@ -5,7 +5,7 @@ REPO_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
 
 pushd $REPO_ROOT
 
-helm repo index stable/ --url https://charts.appscode.com/stable/
+# helm repo index stable/ --url https://charts.appscode.com/stable/
 
 gsutil rsync -d -r stable gs://appscode-charts/stable
 gsutil acl ch -u AllUsers:R -r gs://appscode-charts/stable
