25bed352a3da35711477dc759efe2680105e5421
Tamal Saha <tamal@appscode.com>
2020-06-19 15:29:52 -0700

    Add script to open pr to merge charts
    Signed-off-by: Tamal Saha <tamal@appscode.com>

---
+++ Makefile
@@ -0,0 +1,121 @@
+# Copyright 2019 AppsCode Inc.
+# Copyright 2016 The Kubernetes Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+SHELL=/bin/bash -o pipefail
+
+GO_PKG   := github.com/appscode
+REPO     := $(notdir $(shell pwd))
+BIN      := charts
+COMPRESS ?= no
+
+# This version-strategy uses git tags to set the version string
+git_branch       := $(shell git rev-parse --abbrev-ref HEAD)
+git_tag          := $(shell git describe --exact-match --abbrev=0 2>/dev/null || echo "")
+commit_hash      := $(shell git rev-parse --verify HEAD)
+commit_timestamp := $(shell date --date="@$$(git show -s --format=%ct)" --utc +%FT%T)
+
+VERSION          := $(shell git describe --tags --always --dirty)
+version_strategy := commit_hash
+ifdef git_tag
+	VERSION := $(git_tag)
+	version_strategy := tag
+else
+	ifeq (,$(findstring $(git_branch),master HEAD))
+		ifneq (,$(patsubst release-%,,$(git_branch)))
+			VERSION := $(git_branch)
+			version_strategy := branch
+		endif
+	endif
+endif
+
+###
+### These variables should not need tweaking.
+###
+
+SRC_PKGS :=
+SRC_DIRS := $(SRC_PKGS) hack/scripts # directories which hold app source (not vendored)
+
+# Used internally.  Users should pass GOOS and/or GOARCH.
+OS   := $(if $(GOOS),$(GOOS),$(shell go env GOOS))
+ARCH := $(if $(GOARCH),$(GOARCH),$(shell go env GOARCH))
+
+GO_VERSION       ?= 1.14
+BUILD_IMAGE      ?= appscode/golang-dev:$(GO_VERSION)
+
+# Directories that we need created to build/test.
+BUILD_DIRS  := bin/$(OS)_$(ARCH)     \
+               .go/bin/$(OS)_$(ARCH) \
+               .go/cache
+
+DOCKER_REPO_ROOT := /go/src/$(GO_PKG)/$(REPO)
+
+# If you want to build all binaries, see the 'all-build' rule.
+# If you want to build all containers, see the 'all-container' rule.
+# If you want to build AND push all containers, see the 'all-push' rule.
+all: fmt
+
+fmt: $(BUILD_DIRS)
+	@docker run                                                 \
+	    -i                                                      \
+	    --rm                                                    \
+	    -u $$(id -u):$$(id -g)                                  \
+	    -v $$(pwd):/src                                         \
+	    -w /src                                                 \
+	    -v $$(pwd)/.go/bin/$(OS)_$(ARCH):/go/bin                \
+	    -v $$(pwd)/.go/bin/$(OS)_$(ARCH):/go/bin/$(OS)_$(ARCH)  \
+	    -v $$(pwd)/.go/cache:/.cache                            \
+	    --env HTTP_PROXY=$(HTTP_PROXY)                          \
+	    --env HTTPS_PROXY=$(HTTPS_PROXY)                        \
+	    $(BUILD_IMAGE)                                          \
+	    /bin/bash -c "                                          \
+	        REPO_PKG=$(GO_PKG)                                  \
+	        ./hack/fmt.sh $(SRC_DIRS)                           \
+	    "
+
+$(BUILD_DIRS):
+	@mkdir -p $@
+
+.PHONY: add-license
+add-license:
+	@echo "Adding license header"
+	@docker run --rm 	                                 \
+		-u $$(id -u):$$(id -g)                           \
+		-v /tmp:/.cache                                  \
+		-v $$(pwd):$(DOCKER_REPO_ROOT)                   \
+		-w $(DOCKER_REPO_ROOT)                           \
+		--env HTTP_PROXY=$(HTTP_PROXY)                   \
+		--env HTTPS_PROXY=$(HTTPS_PROXY)                 \
+		$(BUILD_IMAGE)                                   \
+		ltag -t "./hack/license" --excludes "vendor contrib" -v
+
+.PHONY: check-license
+check-license:
+	@echo "Checking files for license header"
+	@docker run --rm 	                                 \
+		-u $$(id -u):$$(id -g)                           \
+		-v /tmp:/.cache                                  \
+		-v $$(pwd):$(DOCKER_REPO_ROOT)                   \
+		-w $(DOCKER_REPO_ROOT)                           \
+		--env HTTP_PROXY=$(HTTP_PROXY)                   \
+		--env HTTPS_PROXY=$(HTTPS_PROXY)                 \
+		$(BUILD_IMAGE)                                   \
+		ltag -t "./hack/license" --excludes "vendor contrib" --check -v
+
+.PHONY: ci
+ci: check-license
+
+.PHONY: clean
+clean:
+	rm -rf .go bin
---
+++ hack/scripts/open-pr.sh
@@ -0,0 +1,157 @@
+#!/bin/bash
+
+# Copyright AppsCode Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -xeou pipefail
+
+SCRIPT_ROOT=$(realpath $(dirname "${BASH_SOURCE[0]}")/..)
+SCRIPT_NAME=$(basename "${BASH_SOURCE[0]}")
+
+pushd $SCRIPT_ROOT
+
+# http://redsymbol.net/articles/bash-exit-traps/
+function cleanup() {
+    popd
+}
+trap cleanup EXIT
+
+# ref: https://gist.github.com/joshisa/297b0bc1ec0dcdda0d1625029711fa24
+parse_url() {
+    proto="$(echo $1 | grep :// | sed -e's,^\(.*://\).*,\1,g')"
+    # remove the protocol
+    url="$(echo ${1/$proto/})"
+
+    IFS='/'                  # / is set as delimiter
+    read -ra PARTS <<<"$url" # str is read into an array as tokens separated by IFS
+    if [ ${PARTS[0]} != 'github.com' ] || [ ${#PARTS[@]} -ne 5 ]; then
+        echo "failed to parse relase-tracker: $url"
+        exit 1
+    fi
+    export RELEASE_TRACKER_OWNER=${PARTS[1]}
+    export RELEASE_TRACKER_REPO=${PARTS[2]}
+    export RELEASE_TRACKER_PR=${PARTS[4]}
+}
+
+if [ -z "$1" ]; then
+    echo "Missing argument for instller directory."
+    echo "Correct usage: $SCRIPT_NAME <path_to_installer_repo> <charts_dir, defaults to charts>."
+    exit 1
+fi
+
+INSTALLER_DIR=$1
+CHARTS_DIR=${2:-charts}
+
+REPO_DIR=stable
+REPO_URL=https://ci-charts.storage.googleapis.com/$REPO_DIR/
+
+cd $INSTALLER_DIR
+
+GIT_TAG=${GITHUB_REF#'refs/tags/'}
+PRODUCT_LINE=${PRODUCT_LINE:-}
+RELEASE=${RELEASE:-}
+RELEASE_TRACKER=${RELEASE_TRACKER:-}
+while IFS=$': \t' read -r marker v; do
+    case $marker in
+        ProductLine)
+            PRODUCT_LINE=$v
+            ;;
+        Release)
+            RELEASE=$v
+            ;;
+        Release-tracker)
+            RELEASE_TRACKER=$v
+            ;;
+    esac
+done < <(git tag -l --format='%(body)' $GIT_TAG)
+
+pr_branch=${GITHUB_REPOSITORY}@${GIT_TAG}
+if [ ! -z "$PRODUCT_LINE" ] && [ ! -z "$RELEASE" ]; then
+    pr_branch=${PRODUCT_LINE}@${RELEASE}
+fi
+
+while true; do
+    cd $SCRIPT_ROOT
+    # remove all unstagged changes
+    git add --all
+    git stash || true
+    git stash drop || true
+    # fetch latest remote
+    git fetch origin --prune
+    git gc
+    # checkout pr branch
+    if [ -z "$(git ls-remote --heads origin $pr_branch)" ]; then
+        # remote branch does NOT exists
+        git checkout master
+        git branch -D $pr_branch || true
+        git checkout -b $pr_branch
+    else
+        git checkout master
+        git branch -D $pr_branch || true
+        git checkout -b $pr_branch --track origin/$pr_branch
+    fi
+    # package charts
+    cd $INSTALLER_DIR
+    find $CHARTS_DIR -maxdepth 1 -mindepth 1 -type d -exec helm package {} -d {} \;
+    mkdir -p $SCRIPT_ROOT/$REPO_DIR
+    if [ -f $SCRIPT_ROOT/$REPO_DIR/index.yaml ]; then
+        helm repo index --merge $SCRIPT_ROOT/$REPO_DIR/index.yaml --url $REPO_URL $CHARTS_DIR
+    else
+        helm repo index --url $REPO_URL $CHARTS_DIR
+    fi
+    mv $CHARTS_DIR/index.yaml $SCRIPT_ROOT/$REPO_DIR/index.yaml
+    cd $CHARTS_DIR
+    find . -maxdepth 1 -mindepth 1 -type d -exec mkdir -p $SCRIPT_ROOT/$REPO_DIR/{} \;
+    find . -path ./$CHARTS_DIR -prune -o -name '*.tgz' -exec mv {} $SCRIPT_ROOT/$REPO_DIR/{} \;
+    # open pr
+    cd $SCRIPT_ROOT
+    git add --all
+    # generate commit command
+    ct_cmd="git commit -a -s -m \"Publish ${GITHUB_REPOSITORY}@${GIT_TAG} charts\""
+    ct_cmd="$ct_cmd --message \"ProductLine: $PRODUCT_LINE\""
+    if [ ! -z "$RELEASE" ]; then
+        ct_cmd="$ct_cmd --message \"Release: $RELEASE\""
+    fi
+    if [ ! -z "$RELEASE_TRACKER" ]; then
+        ct_cmd="$ct_cmd --message \"Release-tracker: $RELEASE_TRACKER\""
+    fi
+    # commit
+    eval "$ct_cmd"
+    # push successfully or { sleep and retry)
+    git push -u origin HEAD || {
+        sleep $((1 + RANDOM % 10))
+        continue
+    }
+    #  open pr
+    pr_cmd=$(
+        cat <<EOF
+hub pull-request \
+    --message "Publish $pr_branch charts" \
+    --message "$(git show -s --format=%b)"
+EOF
+    )
+    # if no Release-tracker: auto merge.
+    if [ -z "$RELEASE_TRACKER" ]; then
+        pr_cmd="$pr_cmd --labels automerge"
+    fi
+    eval "$pr_cmd"
+    # if Release-tracker: found, report back.
+    if [ ! -z "$RELEASE_TRACKER" ]; then
+        parse_url $RELEASE_TRACKER
+        api_url="repos/${RELEASE_TRACKER_OWNER}/${RELEASE_TRACKER_REPO}/issues/${RELEASE_TRACKER_PR}/comments"
+        msg="/chart github.com/${GITHUB_REPOSITORY} ${GIT_TAG}"
+        hub api "$api_url" -f body="$msg"
+    fi
+    exit 0
+done
