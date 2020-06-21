7b51cca8b82e25dc6264c1c7a634e7a5e034827c
Tamal Saha <tamal@appscode.com>
2020-06-20 17:30:24 -0700

    Fix SCRIPT_ROOT path
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- hack/scripts/publish.sh
+++ hack/scripts/publish.sh
@@ -16,7 +16,7 @@
 
 set -eou pipefail
 
-SCRIPT_ROOT=$(realpath $(dirname "${BASH_SOURCE[0]}")/..)
+SCRIPT_ROOT=$(realpath $(dirname "${BASH_SOURCE[0]}")/../..)
 SCRIPT_NAME=$(basename "${BASH_SOURCE[0]}")
 pushd $SCRIPT_ROOT
 
